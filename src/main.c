31f5aec509fed2d15fbbaa7eefb735eb887f9964
Guilherme Lima <guilherme.agl12@gmail.com>
2022-09-17 18:33:33 -0400

    v3

--- src/main.c
+++ src/main.c
@@ -37,34 +37,50 @@ sem_t ConsomeRef, ConsomeY, ConsomeYm, ConsomeYmLinha, ConsomeV, ConsomeX, Conso
 //Gerar Ref
 void* Ref(void*args)
 {
-    clock_t start;
+    struct timespec ts1, ts2, ts3={0};
     double dif;
+    double tm;
     while(t<=TEMPO_MAX)
     {   
-        start=clock();
+        // start=clock();
+
         sem_wait(&ProduzRef);
+            clock_gettime(CLOCK_REALTIME, &ts1);
+            tm = 1000000 * ts1.tv_nsec - tm;
+
             matrix_free(bufferRef);
             bufferRef=defineRef(t);  //verificar t = t*T ()
-            t=t+0.12;
-            //printf("%.4f \n",MVAL(bufferRef,0,0));
-        sem_post(&ConsomeRef);
-        dif=difftime(clock(), start);
-        //JitterRef[contRef]=TEMPO_REF - dif/1000.0;
-        usleep(TEMPO_REF_MS-dif);
+            t=t+TEMPO_REF;
+            printf("%.4f \n",VALUES(bufferRef,0,0));
+            sem_post(&ConsomeRef);
+            dif=t - tm;
+            //JitterRef[contRef]=TEMPO_REF - dif/1000.0;
+            //usleep(TEMPO_REF_MS-dif);
+            
+            clock_gettime(CLOCK_REALTIME, &ts2);
+            ts3.tv_sec = 0;
+            ts3.tv_nsec = T*1000000 - (ts2.tv_nsec - ts1.tv_nsec);
+            nanosleep(&ts3, &ts3);
         contRef++;
     }
 }
 
 void* ModeloRef(void*args)
 {
-    clock_t start;
+    //clock_t start;
+    struct timespec ts1, ts2, ts3={0};
     double dif;
+    double tm;
+    T = TEMPO_MODELO_REF;
     while(t<=TEMPO_MAX)
     {
-        start=clock();
+        // start=clock();
         sem_wait(&ConsomeRef);
         sem_wait(&ProduzYm);
         sem_wait(&ProduzYmLinha);
+            clock_gettime(CLOCK_REALTIME, &ts1);
+            tm = 1000000 * ts1.tv_nsec - tm;
+            t=t+T;
             auxBuffer = matrix_copy(bufferYmLinha);
             matrix_free(bufferYmLinha);
 
@@ -75,9 +91,14 @@ void* ModeloRef(void*args)
         sem_post(&ConsomeYm);
         sem_post(&ConsomeYmLinha);
        
-        dif=difftime(clock(),start);
+            dif=t - tm;
         //JitterModeloRef[contModeloRef]=TEMPO__MODELO_REF - dif/1000.0;
-        usleep(TEMPO_MODELO_REF_MS - dif);
+        //usleep(TEMPO_MODELO_REF_MS - dif);
+
+            clock_gettime(CLOCK_REALTIME, &ts2);
+            ts3.tv_sec = 0;
+            ts3.tv_nsec = T*1000000 - (ts2.tv_nsec - ts1.tv_nsec);
+            nanosleep(&ts3, &ts3);
         contModeloRef++;
     }
 }
@@ -85,60 +106,88 @@ void* ModeloRef(void*args)
 //Bloco Controle
 void* Controle(void*args)
 {
-    clock_t start;
+    struct timespec ts1, ts2, ts3={0};
     double dif;
+    double tm;    
+    T = TEMPO_CONTROLE;
+
     while(t<=TEMPO_MAX)
     {
-        start=clock();
+        // start=clock();
         sem_wait(&ConsomeY);
         sem_wait(&ConsomeYm);
         sem_wait(&ConsomeYmLinha);
         sem_wait(&ProduzV);
+            clock_gettime(CLOCK_REALTIME, &ts1);
+            tm = 1000000 * ts1.tv_nsec - tm;
+            t=t+T;
+            
+
             matrix_free(bufferV);
             bufferV=ControleBloco(bufferYmLinha, bufferYm, bufferY);
         sem_post(&ProduzYm);
         sem_post(&ProduzYmLinha);
         sem_post(&ProduzY);
         sem_post(&ConsomeV);
-        dif=difftime(clock(),start);
+        dif=t - tm;
         // JitterControle[contControle]=TEMPO_CONTROLE-dif/1000.0;
-        usleep(TEMPO_CONTROLE_MS-dif);
+        // usleep(TEMPO_CONTROLE_MS-dif);
+        
+            clock_gettime(CLOCK_REALTIME, &ts2);
+            ts3.tv_sec = 0;
+            ts3.tv_nsec = T*1000000 - (ts2.tv_nsec - ts1.tv_nsec);
+            nanosleep(&ts3, &ts3);
         contControle++;
     }
 }
 
 void* Linear(void*args)
 {
-    clock_t start;
+    struct timespec ts1, ts2, ts3={0};
     double dif;
+    double tm;
+    T = TEMPO_LINEARIZACAO;
     while(t<=TEMPO_MAX)
     {
-        start=clock();
+        //start=clock();
         sem_wait(&ConsomeX);
         sem_wait(&ConsomeV);
         sem_wait(&ProduzU);
+            clock_gettime(CLOCK_REALTIME, &ts1);
+            tm = 1000000 * ts1.tv_nsec - tm;
+            t=t+T;
+            
             matrix_free(bufferU);
             bufferU=Linearizacao(bufferX, bufferV, R);
         sem_post(&ProduzX);
         sem_post(&ProduzV);
         sem_post(&ConsomeU);
-        dif= difftime(clock(),start);
+        dif= t - tm;
         // JitterLinearizacao[contLinearizacao]=TEMPO_LINEARIZACAO -dif/1000.0;
-        usleep(TEMPO_LINEARIZACAO_MS-dif);
+        // usleep(TEMPO_LINEARIZACAO_MS-dif);
+            clock_gettime(CLOCK_REALTIME, &ts2);
+            ts3.tv_sec = 0;
+            ts3.tv_nsec = T*1000000 - (ts2.tv_nsec - ts1.tv_nsec);
+            nanosleep(&ts3, &ts3);
         contLinearizacao++;
     }
 }
 
 void* Robo(void*args)
 {
-    clock_t start;
+    struct timespec ts1, ts2, ts3={0};
     double dif;
+    double tm;
+    T = TEMPO_ROBO;
     while(t<=TEMPO_MAX)
     {
-        start=clock();
         sem_wait(&ConsomeU);
         sem_wait(&ProduzX);
         sem_wait(&ProduzY);
+            clock_gettime(CLOCK_REALTIME, &ts1);
+            tm = 1000000 * ts1.tv_nsec - tm;
+            t=t+T;
+
             printf("%.2lf, %lf, %lf, %lf\n", t-0.12 ,matrix_get_value(bufferX,0,0) ,matrix_get_value(bufferX,1,0),matrix_get_value(bufferX,2,0));
             auxBuffer2 = matrix_copy(XtLinha);
             
@@ -153,9 +202,14 @@ void* Robo(void*args)
         sem_post(&ProduzU);
         sem_post(&ConsomeX);
         sem_post(&ConsomeY);
-        dif=difftime(clock(),start);
+        dif=t - tm;
         // JitterRobo[contRobo]=TEMPO_ROBO - dif/1000.0;
-        usleep(TEMPO_ROBO_MS-dif);
+        // usleep(TEMPO_ROBO_MS-dif);
+
+            clock_gettime(CLOCK_REALTIME, &ts2);
+            ts3.tv_sec = 0;
+            ts3.tv_nsec = T*1000000 - (ts2.tv_nsec - ts1.tv_nsec);
+            nanosleep(&ts3, &ts3);
         contRobo++;
     }
 
